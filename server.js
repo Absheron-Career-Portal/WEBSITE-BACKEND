f05ec73e19e74a6a1b26da1e44660030d51ec319
kenanmusali <textme0306@gmail.com>
2025-11-13 13:16:07 +0400

    Updated

--- server.js
+++ server.js
@@ -88,5 +88,5 @@ app.use((req, res) => {
 // Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
